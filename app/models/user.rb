636c53c3e2b30ca2b0b8dd45f03914923d80465f
nilesh-kumar-sahu <nileshkumar42490@gmail.com>
2022-02-22 00:00:48 +0530

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                 format: { with: VALID_EMAIL_REGEX },
                 uniqueness: true)
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -37,6 +37,6 @@ class User < ApplicationRecord
   # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
-  end  
+  end
 
 end
